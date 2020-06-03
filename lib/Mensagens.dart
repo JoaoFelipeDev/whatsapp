408c96098218c007b1c3c325c916656bf0c5a94f
João Felipe Moisés Lúcio Ribeiro <alucardspecial@gmail.com>
2020-06-02 21:30:22 -0300

    Enviar imagem
    Criei a função de enviar imagen

--- lib/Mensagens.dart
+++ lib/Mensagens.dart
@@ -3,6 +3,9 @@ import 'package:whatsapp/model/Mensagem.dart';
 import 'package:whatsapp/model/Usuario.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
 class Mensagens extends StatefulWidget {
   Usuario contato;
@@ -14,25 +17,12 @@ class Mensagens extends StatefulWidget {
 }
 
 class _MensagensState extends State<Mensagens> {
+  bool _subindoImagem = false;
+  File _imagem;
   String _idUsuarioLogado;
   String _idUsuarioDestinatario;
   Firestore db = Firestore.instance;
 
-  List<String> listaMensagens = [
-    "Olá meu amigo, tudo bem?",
-    "Tudo ótimo!!! e contigo?",
-    "Estou muito bem!! queria ver uma coisa contigo, você vai na corrida de sábado?",
-    "Não sei ainda :(",
-    "Pq se você fosse, queria ver se posso ir com você...",
-    "Posso te confirma no sábado? vou ver isso",
-    "Opa! tranquilo",
-    "Excelente!!",
-    "Estou animado para essa corrida, não vejo a hora de chegar! ;) ",
-    "Vai estar bem legal!! muita gente",
-    "vai sim!",
-    "Lembra do carro que tinha te falado",
-    "Que legal!!"
-  ];
   TextEditingController _controllerMensagem = TextEditingController();
 
   _enviarMensagem() {
@@ -61,18 +51,62 @@ class _MensagensState extends State<Mensagens> {
     //Limpa texto
     _controllerMensagem.clear();
 
-    /*
-
-    + mensagens
-      + joao
-        + Gatin
-          + identicadorFirebase
-            <Mensagem>
-
-    * */
   }
 
-  _enviarFoto() {}
+  _enviarFoto() async {
+    File imagemSelecionada;
+    imagemSelecionada = await ImagePicker.pickImage(source: ImageSource.gallery);
+     _subindoImagem = true;
+    String _nomeImagem = DateTime.now().millisecondsSinceEpoch.toString();
+
+    FirebaseStorage storage = FirebaseStorage.instance;
+    StorageReference pastaRaiz = storage.ref();
+    StorageReference arquivo = pastaRaiz
+        .child("mensagens")
+        .child(_idUsuarioLogado)
+        .child(_nomeImagem + ".jpg");
+
+    //Upload da imagem
+    StorageUploadTask task = arquivo.putFile(imagemSelecionada);
+
+    //Controlar progresso do upload
+    task.events.listen((StorageTaskEvent storageEvent){
+
+      if( storageEvent.type == StorageTaskEventType.progress ){
+        setState(() {
+          _subindoImagem = true;
+        });
+      }else if( storageEvent.type == StorageTaskEventType.success ){
+        setState(() {
+          _subindoImagem = false;
+        });
+      }
+
+    });
+
+    //Recuperar url da imagem
+    task.onComplete.then((StorageTaskSnapshot snapshot){
+      _recuperarUrlImagem(snapshot);
+    });
+
+
+  }
+  Future _recuperarUrlImagem(StorageTaskSnapshot snapshot) async {
+
+    String url = await snapshot.ref.getDownloadURL();
+
+    Mensagem mensagem = Mensagem();
+    mensagem.idUsuario = _idUsuarioLogado;
+    mensagem.mensagem = "";
+    mensagem.urlImagem = url;
+    mensagem.tipo = "imagem";
+
+    _salvarMensagem(_idUsuarioLogado, _idUsuarioDestinatario, mensagem);
+    //Salvando msg para o destinatatio
+    _salvarMensagem(_idUsuarioDestinatario, _idUsuarioLogado, mensagem);
+
+
+  }
 
   _recuperarDadosUsuario() async {
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -109,8 +143,11 @@ class _MensagensState extends State<Mensagens> {
                     fillColor: Colors.white,
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(32)),
-                    prefixIcon: IconButton(
-                        icon: Icon(Icons.camera_alt), onPressed: _enviarFoto)),
+                    prefixIcon:
+                    _subindoImagem
+                    ? CircularProgressIndicator()
+                    : IconButton(icon: Icon(Icons.camera_alt), onPressed: _enviarFoto)
+                ),
               ),
             ),
           ),
@@ -148,7 +185,6 @@ class _MensagensState extends State<Mensagens> {
             break;
           case ConnectionState.active:
           case ConnectionState.done:
-
             QuerySnapshot querySnapshot = snapshot.data;
 
             if (snapshot.hasError) {
@@ -160,9 +196,9 @@ class _MensagensState extends State<Mensagens> {
                 child: ListView.builder(
                     itemCount: querySnapshot.documents.length,
                     itemBuilder: (context, indice) {
-
                       //recupera mensagem
-                      List<DocumentSnapshot> mensagens = querySnapshot.documents.toList();
+                      List<DocumentSnapshot> mensagens =
+                          querySnapshot.documents.toList();
                       DocumentSnapshot item = mensagens[indice];
 
                       double larguraContainer =
@@ -171,7 +207,7 @@ class _MensagensState extends State<Mensagens> {
                       //Define cores e alinhamentos
                       Alignment alinhamento = Alignment.centerRight;
                       Color cor = Color(0xffd2ffa5);
-                      if ( _idUsuarioLogado != item["idUsuario"] ) {
+                      if (_idUsuarioLogado != item["idUsuario"]) {
                         alinhamento = Alignment.centerLeft;
                         cor = Colors.white;
                       }
@@ -186,11 +222,12 @@ class _MensagensState extends State<Mensagens> {
                             decoration: BoxDecoration(
                                 color: cor,
                                 borderRadius:
-                                BorderRadius.all(Radius.circular(8))),
-                            child: Text(
-                              item["mensagem"],
-                              style: TextStyle(fontSize: 18),
-                            ),
+                                    BorderRadius.all(Radius.circular(8))),
+                            child:
+                            item ["tipo"] == "texto"
+                            ? Text(item["mensagem"], style: TextStyle(fontSize: 18),)
+                                : Image.network(item["urlImagem"]),
+
                           ),
                         ),
                       );
@@ -201,44 +238,6 @@ class _MensagensState extends State<Mensagens> {
             break;
         }
       },
-    );
-
-    var listView = Expanded(
-      child: ListView.builder(
-          itemCount: listaMensagens.length,
-          itemBuilder: (context, indice) {
-            double larguraContainer = MediaQuery.of(context).size.width * 0.8;
-
-            //larguraContainer -> 100
-            //x                -> 80
-
-            //Define cores e alinhamentos
-            Alignment alinhamento = Alignment.centerRight;
-            Color cor = Color(0xffd2ffa5);
-            if (indice % 2 == 0) {
-              //par
-              alinhamento = Alignment.centerLeft;
-              cor = Colors.white;
-            }
-
-            return Align(
-              alignment: alinhamento,
-              child: Padding(
-                padding: EdgeInsets.all(6),
-                child: Container(
-                  width: larguraContainer,
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                      color: cor,
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  child: Text(
-                    listaMensagens[indice],
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ),
-            );
-          }),
     );
 
     return Scaffold(
@@ -265,16 +264,15 @@ class _MensagensState extends State<Mensagens> {
                 image: AssetImage("imagens/bg.png"), fit: BoxFit.cover)),
         child: SafeArea(
             child: Container(
-              padding: EdgeInsets.all(8),
-              child: Column(
-                children: <Widget>[
-                  stream,
-                  caixaMensagem,
-                ],
-              ),
-            )),
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              stream,
+              caixaMensagem,
+            ],
+          ),
+        )),
       ),
     );
   }
 }
-
